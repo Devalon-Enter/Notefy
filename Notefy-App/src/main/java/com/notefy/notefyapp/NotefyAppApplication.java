f51afb714f47373b9ff1bf080d3ba1ed9d6b44f1
Devalon-Enter <faberlorin@hotmail.com>
2023-09-21 16:43:13 +0200

    springboot added to the project

---
+++ Notefy-App/src/main/java/com/notefy/notefyapp/NotefyAppApplication.java
@@ -0,0 +1,12 @@
+package com.notefy.notefyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotefyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotefyAppApplication.class, args);
+	}
+}
