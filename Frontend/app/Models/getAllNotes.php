2d76bc1bacac64561d996f5282010f9f98965854
Da17iel <68570543+Da17iel@users.noreply.github.com>
2023-10-06 19:06:31 +0200

    BugFix: Can now show task status

--- Frontend/app/Models/getAllNotes.php
+++ Frontend/app/Models/getAllNotes.php
@@ -29,7 +29,7 @@ foreach ($decodedData as $item) {
     // Format the HTML Table
     echo "<td>" . $item["title"] . "</td>";
     echo "<td>" . $item["priority"] . "</td>";
-    echo "<td>" . $item["done"] . "</td>";      // Somehow does not work yet
+    echo "<td>" . (($item["done"] == 1) ? "Done" : "Not Done") . "</td>";
     echo "<td>" . $item["createDate"] . "</td>";
     echo "<td>" . $item["dueDate"] . "</td>";
     echo "<td>" . deleteButton($item["id"]) . "</td>";
