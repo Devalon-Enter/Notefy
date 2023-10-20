06a7fd94f277be946e0b11d51ac5e05b12569a3a
Da17iel <68570543+Da17iel@users.noreply.github.com>
2023-10-21 00:37:47 +0200

    Frontend: Updated showTasks files

--- Frontend/app/Models/showDueTasks.php
+++ Frontend/app/Models/showDueTasks.php
@@ -2,11 +2,14 @@
 include "../app/Controller/TaskController.php";
 
 foreach (getAllDueTasks() as $DueTask) {
-    echo "<div class='dueTaskBox'>";
-    echo "<p>" . $DueTask["title"] ."</p>";
-    echo "<p>" . $DueTask["description"] . "</p>";
-    echo "<p>" . $DueTask["priority"] . "</p>";
-    echo "<p>" . $DueTask["createDate"] . "</p>";
-    echo "<p>" . $DueTask["dueDate"] . "</p>";
+    echo "<div class='taskBox'>";
+    echo "<p class='taskBoxTitle'>" . $DueTask["title"] ."</p>";
+    echo "<p class='taskBoxCreateDate'>Created at: " . $DueTask["createDate"] . "</p>";
+    echo "<p class='taskBoxDescription'>" . $DueTask["description"] . "</p>";
+    echo "<p class='taskBoxPriority " . $DueTask["priority"] . "'>" . $DueTask["priority"] . "</p>";
+    echo "<span class='taskBoxDueDate'>Due to: " . $DueTask["dueDate"] . "</span>";
+    echo "<button class='editButton'>Edit</button>";
+    echo "<button class='deleteButton'>Delete</button>";
+    echo "<button class='doneButton'>✔</button>";
     echo "</div>";
 }
