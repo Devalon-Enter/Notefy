f622bd73d82daadfb135433f802440e8812ca5b4
Da17iel <68570543+Da17iel@users.noreply.github.com>
2023-10-21 00:33:57 +0200

    Updated interface and CSS code

--- Frontend/public/index.php
+++ Frontend/public/index.php
@@ -5,52 +5,51 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <!-- Google Fonts end -->
     <link rel="stylesheet" href="style.css">
     <title>HomePage</title>
 </head>
 <body>
     <div class="landingScreen">
-        <h1>Notefy</h1>
+        <h1 class="pageTitle">Notefy</h1>
 
-        <div class="taskBox">
-            <h2>Create a new Task</h2>
+        <div class="createTaskBox">
+            <div class="createTaskBoxContainer">
+                <h2>Create a new Task</h2>
 
-            <form action="../app/Models/createTask.php" method="post">
-                <label for="title">Title:</label>
-                <input type="text" name="title" id="title" required autofocus>
-                <br>
+                <form action="../app/Models/createTask.php" method="post">
+                    <label for="title">Title:</label><br>
+                    <input type="text" name="title" id="title" required autofocus>
+                    <br>
 
-                <label for="description">Description:</label>
-                <textarea name="description" id="description"></textarea>
-                <br>
+                    <label for="description">Description:</label><br>
+                    <textarea name="description" id="description"></textarea>
+                    <br>
 
-                <label for="priority">Priority:</label>
-                <select name="priority" id="priority" required>
-                    <option value="CRITICAL">CRITICAL</option>
-                    <option value="HIGH">HIGH</option>
-                    <option value="MINOR">MINOR</option>
-                    <option value="LOW">LOW</option>
-                </select>
-                <br>
+                    <label for="priority">Priority:</label><br>
+                    <select name="priority" id="priority" required>
+                        <option value="CRITICAL">CRITICAL</option>
+                        <option value="HIGH">HIGH</option>
+                        <option value="MINOR" selected>MINOR</option>
+                        <option value="LOW">LOW</option>
+                    </select>
+                    <br>
 
-                <label for="dueDate">Due Date:</label>
-                <input type="date" name="dueDate" id="dueDate" required>
-                <br>
+                    <label for="dueDate">Due Date:</label><br>
+                    <input type="date" name="dueDate" id="dueDate" required>
+                    <br>
 
-                <input type="submit" value="Submit">
-            </form>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
         </div>
+
     </div>
 
 
     <div class="showTasks">
+        <h2 class="pageTitle">All tasks</h2>
+
         <div class="dueTasks">
-            <h2>All tasks</h2>
 
             <?php
                 include '../app/Models/showDueTasks.php';
