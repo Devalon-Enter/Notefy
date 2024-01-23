f322ca9c06ebab0fcb8410107da3336f314ead04
Da17iel <68570543+Da17iel@users.noreply.github.com>
2024-01-23 17:18:42 +0100

    Created logger class for application logging

---
+++ Notefy-App/src/main/java/com/notefy/notefyapp/logger/LogType.java
@@ -0,0 +1,12 @@
+package com.notefy.notefyapp.logger;
+
+public enum LogType {
+    DEBUG,
+    INFO,
+    NOTICE,
+    WARNING,
+    ERROR,
+    CRITICAL,
+    ALERT,
+    EMERGENCY
+}
---
+++ Notefy-App/src/main/java/com/notefy/notefyapp/logger/Logger.java
@@ -0,0 +1,37 @@
+package com.notefy.notefyapp.logger;
+
+import java.util.Date;
+
+public class Logger {
+    private static Logger instance;
+    private int counter = 0;
+
+    // Private constructor to prevent instantiation outside the class
+    private Logger() {
+
+    }
+
+    // Public method to get the singleton instance
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // Method to log messages
+    public void log(String message, LogType logType) {
+        counter += 1;
+        Date currentDateTime = new Date();
+        System.out.println(
+                "ID:" + counter + "; " +
+                "Message:\"" + message + "\"; " +
+                "Date:" + currentDateTime + "; " +
+                "Type:" + logType.toString() + ";"
+        );
+    }
+}
