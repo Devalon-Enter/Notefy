c2a62d36d2e33a3705ab4db1757759a237729c67
Devalon-Enter <faberlorin@hotmail.com>
2024-01-26 16:29:16 +0100

    Authentication and Authorization implemented and usable

---
+++ Notefy-App/src/main/java/com/notefy/notefyapp/config/TaskConfig.java
@@ -0,0 +1,51 @@
+package com.notefy.notefyapp.task;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.notefy.notefyapp.task.PriorityType.HIGH;
+import static com.notefy.notefyapp.task.PriorityType.LOW;
+import static java.util.Calendar.*;
+
+/**
+ * Da unsere Applikation nicht durchgehend läuft, wird hier
+ * jedes Mal wenn sie gestartet wird, zwei neue Objekte auf die Datenbank geschrieben.
+ * Das soll uns Zeit einsparen.
+ * @author Lorin Faber
+ * @version 1.0.0
+ */
+@Configuration
+@Profile("prod")
+public class TaskConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(TaskRepository repository) {
+        return args -> {
+            Task n1 = new Task(
+                    "Create Notes",
+                    HIGH,
+                    false,
+                    LocalDate.of(2023, SEPTEMBER, 21),
+                    LocalDate.of(2023, SEPTEMBER, 23),
+                    "We need for some tests those notes alright?"
+            );
+
+            Task n2 = new Task(
+                    "Update Database",
+                    LOW,
+                    false,
+                    LocalDate.of(2023, SEPTEMBER, 21),
+                    LocalDate.of(2023, SEPTEMBER, 25),
+                    "In order for us to work, we need to be sure the DB is updated."
+            );
+
+            repository.saveAll(
+                    List.of(n1, n2)
+            );
+        };
+    }
+}
---
+++ Notefy-App/src/main/java/com/notefy/notefyapp/user/UserConfig.java
@@ -0,0 +1,2 @@
+package com.notefy.notefyapp.user;public class UserConfig {
+}
